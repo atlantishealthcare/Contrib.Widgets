eac7a515bb40ffbdc40b5844cdabd05c923b4405
Nicholas Mayne <Jetski5822@gmail.com>
2013-11-29 16:03:36 +0000

    fixing for 172

--- Controllers/AdminController.cs
+++ Controllers/AdminController.cs
@@ -6,7 +6,6 @@ using Contrib.Widgets.Models;
 using Contrib.Widgets.Services;
 using Orchard;
 using Orchard.ContentManagement;
-using Orchard.Core.Contents.Controllers;
 using Orchard.Environment.Extensions;
 using Orchard.Localization;
 using Orchard.Logging;
